9acba8e7d82bacecd9edc5a9801bc923b4ebcef4
Klauztrofobi <97224194+Klauztrofobi@users.noreply.github.com>
2022-06-08 11:41:20 +0200

    Add files via upload

---
+++ Profile.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kodetime
+{
+    internal class Profile
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Bosted { get; set; }
+        public string Bursdag { get; set; }
+        public bool Loggedin { get; set; }
+        public List<Profile> Friends { get; set; }
+
+        public Profile(string name, int age, string bosted, string bursdag, bool loggedin)
+        {
+            Name = name;
+            Age = age;
+            Bosted = bosted;
+            Bursdag = bursdag;
+            Loggedin = loggedin;
+
+        }
+
+        public List<Profile> addFriend(Profile user)
+        {
+
+            //Friends = new Profile[] { user };
+            Friends.Add(user);
+            return Friends;
+        }
+
+        public string showFriends()
+        {
+            string output = "";
+           
+            for(var i = 0; i < Friends.Count; i++)
+            {
+                output += Friends[i].Name;
+            }
+            return output;
+          
+            //for (var i = 0; i < Friends.Length; i++)
+            //{
+            //    output += Friends[i].Name;
+            //}
+            //return output;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+
+namespace Kodetime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var therese = new Profile("Therese", 32, "sandefjord", "02.04", true);
+            var Thomas = new Profile("Thomas", 30, "Sandefjord", "16.08", false);
+            var Truls = new Profile("Truls", 24, "tbg", "06.08", true);
+
+
+            //Profile[] alleprofiler = new Profile[] { therese, Thomas };
+
+            //therese.addFriend(Thomas);
+            //therese.addFriend(Truls);
+            //Console.WriteLine(therese.showFriends());
+            //for(int i = 0;i < alleprofiler.Length;i++)
+            //{
+                //Console.WriteLine(alleprofiler[i].Name);
+            //}
+
+            var AddFriend = new List<Profile>();
+            AddFriend.Add(therese);
+            AddFriend.Add(Thomas);
+
+            
+        }
+    }
+}
